672f86c8b6823fb143b66d1f6c04b70cbcc804a5
Pat Brown <ptrckbrown94@gmail.com>
2019-12-18 17:24:09 -0600

    updated db data and started with menu options

--- db/seed.sql
+++ db/seed.sql
@@ -10,4 +10,4 @@ VALUE ("manager", 75000.00, 2), ("engineer", 52000, 3), ("accountant", 62500, 4)
 
 --add employee
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUE ("John", "Smith", )
+VALUE ("John", "Smith", 1, null), ("Jill", "Green", 1, 1), ("Jack", "Brown", 3, 2), ("Steve", "Quinn", 5, 2);
--- server.js
+++ server.js
@@ -3,3 +3,102 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
 const cTable = require("console.table")
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "company_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+    menu()
+    //  connection.end();//
+});
+
+/*
+
+*/
+function menu() {
+    inquirer.prompt({
+        type: "list",
+        choices: ["Add department", "Add role", "Add employee", "View departments", "View roles", "View employees", "Update employee role", "Quit"],
+        message: "What would you like to do?",
+        name: "option"
+    })
+        .then(function (result) {
+            console.log("You entered: " + result.option)
+
+            switch (result.option) {
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Add role":
+                    viewDepartment();
+                    break;
+                case "Add employee":
+                    viewDepartment();
+                    break;
+                case "View departments":
+                    viewDepartment();
+                    break;
+                case "View roles":
+                    viewDepartment();
+                    break;
+                case "View employees":
+                    viewDepartment();
+                    break;
+                case "Update employee role":
+                    viewDepartment();
+                    break;
+                default:
+                    quit()
+
+            }
+
+
+        });
+
+}
+
+function addDepartment() {
+ // inquiere to get the inputs (name)
+ // execute the sql (INSERT)
+
+}
+
+function viewDepartment() {
+    // select from the db
+    var query = "SELECT * FROM department"
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res)
+        menu()
+
+    })
+    // show the result to the user (console.table)
+
+}
+
+
+
+
+
+
+
+function quit() {
+
+    connection.end()
+    process.exit()
+
+}
